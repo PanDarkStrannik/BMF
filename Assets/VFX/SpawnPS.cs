b2a9bc51a07ad7f190f094e6d8cb463b339703a5
Руслан Черныш <ruslanchernysh1134@gmail.com>
2020-09-14 17:48:33 +0300

    Church

---
+++ Assets/VFX/SpawnPS.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPS : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem rocks;
+    [SerializeField] private float rockTime;
+
+    private void Update()
+    {
+        if (rockTime <= 0)
+        {
+            var main = rocks.main;
+            main.gravityModifier = 4;
+        }
+        else rockTime -= Time.deltaTime;
+    }
+}
