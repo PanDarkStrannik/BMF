8757a1b0aa86d56aa776d6269887aed766fe357e
DanilA-dev <78906212+DanilA-dev@users.noreply.github.com>
2021-02-23 03:34:41 +0300

    Небольшой апдейт
    Holy Water

---
+++ Assets/Scripts/New Scripts/UI Related/WeaponUI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeaponUI : MonoBehaviour
+{
+    [SerializeField] private Text ammoAmout;
+
+    private void OnEnable()
+    {
+        PlayerInformation.GetInstance().PlayerParamController.DamagebleParams.OnParamChanged += UpdateAmmo;
+    }
+
+    private void OnDisable()
+    {
+        PlayerInformation.GetInstance().PlayerParamController.DamagebleParams.OnParamChanged -= UpdateAmmo;
+    }
+
+
+    public void UpdateAmmo(DamagebleParam.ParamType paramType, float value, float maxValue)
+    {
+
+      if(paramType == DamagebleParam.ParamType.HolyWater)
+        {
+            if(value < maxValue)
+            {
+                ammoAmout.text = value.ToString();
+            }
+        }
+    }
+
+}
