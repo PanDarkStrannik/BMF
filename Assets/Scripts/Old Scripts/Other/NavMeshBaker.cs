47ddf49c11061d76550e954862ae2f96c1d613e6
PanDarkStrannik <49794843+PanDarkStrannik@users.noreply.github.com>
2021-04-08 02:26:59 +0700

    Починил навмеш при разрушении

--- Assets/Scripts/Old Scripts/Other/NavMeshBaker.cs
+++ Assets/Scripts/Old Scripts/Other/NavMeshBaker.cs
@@ -2,35 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Events;
 
 public class NavMeshBaker : MonoBehaviour
 {
+    [SerializeReference] private List<NewDestruct> destructs;
+    [SerializeReference] private List<NavMeshSurface> navMeshes;
+    [SerializeField] private UnityEvent OnGenerateEnd;
 
-    [SerializeField] List<NavMeshSurface> navMeshes;
-    [SerializeField] List<SimpleDestruct> simpleDestructs;
-    [SerializeField] private float toBakeTime = 3f;
-
-    private void Start()
+    private void OnEnable()
     {
-        foreach(var destrcut in simpleDestructs)
+        foreach(var e in destructs)
         {
-            destrcut.DestructEvent += delegate
-            {
-                StartCoroutine(GenerateMesh());
-            };
+            e.OnDestruct.AddListener(GenerateMesh);
+        }
+    }
+    private void OnDisable()
+    {
+        foreach (var e in destructs)
+        {
+            e.OnDestruct.RemoveListener(GenerateMesh);
         }
     }
 
-    public IEnumerator GenerateMesh()
+    public void GenerateNavmeshByTime(float time)
     {
-        yield return new WaitForSecondsRealtime(toBakeTime);
-        foreach(var nav in navMeshes)
+        StartCoroutine(GenerateMesh(time));
+    }
+
+    private IEnumerator GenerateMesh(float time)
+    {
+        yield return new WaitForSecondsRealtime(time);
+        GenerateMesh();
+    }
+
+    private void GenerateMesh()
+    {
+        foreach (var nav in navMeshes)
         {
             if (nav.gameObject.activeSelf)
             {
                 nav.BuildNavMesh();
             }
         }
+        OnGenerateEnd?.Invoke();
     }
 
 }
