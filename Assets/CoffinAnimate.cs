f46a3493d04664f5ad349bbb3331bce9196f306d
DanilA-dev <78906212+DanilA-dev@users.noreply.github.com>
2021-04-13 20:19:30 +0300

    сцена с гробом

---
+++ Assets/CoffinAnimate.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+
+public class CoffinAnimate : MonoBehaviour
+{
+    public UnityEvent OnButtonPressed;
+
+    private PlayerInput input;
+
+    private void Awake()
+    {
+        input = new PlayerInput();
+        input.ButtonInputs.Heal.performed += _ => PlayAnimation();
+    }
+
+    private void OnEnable()
+    {
+        input.Enable();
+    }
+    private void OnDisable()
+    {
+        input.Disable();
+    }
+
+    private void PlayAnimation()
+    {
+        OnButtonPressed?.Invoke();
+    }
+
+
+}
--- Assets/Plugins/VolumetricLightBeam/Shaders/VLBGeneratedShader.shader
+++ Assets/Plugins/VolumetricLightBeam/Shaders/VLBGeneratedShader.shader
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:01ef1fb3f6feed9da6f7a5d64a7b3555fde3efb5b95cb72f312c4876531c0568
-size 4270
+oid sha256:a10e26dc22d5854968251fcd951fc26f742a3ee981db5dbbc7379195a451313c
+size 4229
